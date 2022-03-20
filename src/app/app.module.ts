115a00815a753bf5fd7b09841930a985aeb0658b
aminLuis <aminluisamin@gmail.com>
2022-03-20 15:46:32 -0500

    Inicio aplicacion web Angular 13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    ClienteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'cliente-component', component: ClienteComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
